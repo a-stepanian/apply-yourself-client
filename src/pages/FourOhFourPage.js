7c9adac02ad545ebc977e7dac7e4bf4e9982c1a0
Alex Stepanian <alexstepanian11@gmail.com>
2022-09-28 10:00:00 -0700

    add 404 page and route

---
+++ src/pages/FourOhFourPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const FourOhFourPage = () => {
+  return (
+    <Wrapper>
+      <p className="four-oh-four">404</p>
+      <p className="not-found">Page Not Found</p>
+      <div className="img-wrapper">
+        <img src="/notfound.svg" alt="Alien getting abducted into spaceship." />
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  background-color: var(--dark-blue);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3rem;
+  p {
+    color: var(--yellow);
+    font-family: "Josefin Slab", serif;
+    font-weight: 900;
+  }
+  .four-oh-four {
+    font-size: 4rem;
+  }
+  .not-found {
+    font-size: 1.2rem;
+  }
+  .img-wrapper {
+    padding: 1rem 0 0 1rem;
+    width: 100%;
+    max-width: 30rem;
+    display: flex;
+    justify-content: center;
+    img {
+      width: 100%;
+    }
+  }
+`;
+export default FourOhFourPage;
