7de51fd56ead3cbc921f02d61324e7c278ecb9c5
Alex Stepanian <alexstepanian11@gmail.com>
2024-08-15 22:00:16 -0700

    Update Loading UI and metadata

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,60 +1,24 @@
 import styled from "styled-components";
-import { LoadingSpinner } from "./LoadingSpinner";
 import { TypingEffect } from "./TypingEffect";
 import { useState } from "react";
 
 interface IShow {
-  line0: boolean;
   line1: boolean;
   line2: boolean;
-  line3: boolean;
 }
 
 export const Loading = () => {
-  const [show, setShow] = useState<IShow>({
-    line0: false,
-    line1: false,
-    line2: false,
-    line3: false
-  });
+  const [show, setShow] = useState<IShow>({ line1: false, line2: false });
 
-  setTimeout(() => {
-    setShow(prev => {
-      return { ...prev, line0: true };
-    });
-  }, 50);
-  setTimeout(() => {
-    setShow(prev => {
-      return { ...prev, line1: true };
-    });
-  }, 800);
-  setTimeout(() => {
-    setShow(prev => {
-      return { ...prev, line2: true };
-    });
-  }, 1000);
-  setTimeout(() => {
-    setShow(prev => {
-      return { ...prev, line3: true };
-    });
-  }, 1200);
+  setTimeout(() => setShow(prev => ({ ...prev, line1: true })), 50);
+  setTimeout(() => setShow(prev => ({ ...prev, line2: true })), 500);
+
+  const wavePattern = "⋅.˳˳.⋅˙ॱ˙⋅.˳˳.⋅⋅.˳˳.⋅˙ॱ˙".repeat(30);
 
   return (
     <Wrapper>
-      <LoadingSpinner />
-      {show.line0 && <TypingEffect text="Loading results..." textElementType="p" speedInMilliseconds={10} />}
-
-      {show.line1 && (
-        <TypingEffect
-          text="Waking up the server................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................."
-          textElementType="p"
-          speedInMilliseconds={10}
-        />
-      )}
-      {/* {show.line2 && <TypingEffect text="The upside is that it's free!" textElementType="p" speedInMilliseconds={20} />}
-      {show.line3 && (
-        <TypingEffect text="Thank you for your patience :)" textElementType="p" speedInMilliseconds={20} />
-      )} */}
+      {show.line1 && <TypingEffect text="Loading results..." textElementType="p" speedInMilliseconds={10} />}
+      {show.line2 && <TypingEffect text={wavePattern} textElementType="p" speedInMilliseconds={40} />}
     </Wrapper>
   );
 };
@@ -66,6 +30,13 @@ const Wrapper = styled.div`
   align-items: center;
   padding-top: 2rem;
   p {
-    margin: 2rem 0;
+    margin: 1rem 0;
+    color: ${({ theme }) => theme.primaryPink};
+    font-family: "Poppins", sans-serif;
+    font-size: 1rem;
+    &:nth-of-type(2) {
+      font-weight: 300;
+      font-size: 1.5rem;
+    }
   }
 `;
