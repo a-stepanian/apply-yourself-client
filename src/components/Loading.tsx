d2e8562bfa5faf23a90e4d015f2a1b0485186b44
Alex Stepanian <alexstepanian11@gmail.com>
2024-08-15 20:44:49 -0700

    Update text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -27,35 +27,31 @@ export const Loading = () => {
     setShow(prev => {
       return { ...prev, line1: true };
     });
-  }, 300);
+  }, 800);
   setTimeout(() => {
     setShow(prev => {
       return { ...prev, line2: true };
     });
-  }, 600);
+  }, 1000);
   setTimeout(() => {
     setShow(prev => {
       return { ...prev, line3: true };
     });
-  }, 900);
+  }, 1200);
 
   return (
     <Wrapper>
       <LoadingSpinner />
-      {show.line0 && (
-        // <TypingEffect text="Waking up the server, just a minute..." textElementType="p" speedInMilliseconds={20} />
-        <TypingEffect text="Loading results..." textElementType="p" speedInMilliseconds={20} />
-      )}
+      {show.line0 && <TypingEffect text="Loading results..." textElementType="p" speedInMilliseconds={10} />}
 
-      {/* {show.line1 && (
+      {show.line1 && (
         <TypingEffect
-          text="The downside of hosting the backend of this site on a free tiered service is that it goes to sleep after a
-        period of inactivity."
+          text="Waking up the server................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................."
           textElementType="p"
-          speedInMilliseconds={20}
+          speedInMilliseconds={10}
         />
       )}
-      {show.line2 && <TypingEffect text="The upside is that it's free!" textElementType="p" speedInMilliseconds={20} />}
+      {/* {show.line2 && <TypingEffect text="The upside is that it's free!" textElementType="p" speedInMilliseconds={20} />}
       {show.line3 && (
         <TypingEffect text="Thank you for your patience :)" textElementType="p" speedInMilliseconds={20} />
       )} */}
